a307c183e1ccf80842de2b2b7a23eaa5a089e6c2
hoandzk2 <hoan2108@icloud.com>
2025-05-23 14:40:40 +0900

    1

--- server-data/resources/[qb]/qb-spawn/config.lua
+++ server-data/resources/[qb]/qb-spawn/config.lua
@@ -3,7 +3,8 @@ QB = {}
 QB.Spawns = {
     motel = {
         coords = vector4(327.56, -205.08, 53.08, 163.5),
-        location = 'motel',
-        label = 'Motels',
+        location = 'Nhà Nghỉ',
+        label = 'Nhà Nghỉ',
     },
 }
+
